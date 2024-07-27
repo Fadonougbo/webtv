a732dea0d109ca337cf5086a7db138d631457ba6
Fadonougbo <fadougbogautier@gmail.com>
2024-07-27 23:11:13 +0100

    mise en form hoe

---
+++ resources/views/webtv/home/home.blade.php
@@ -0,0 +1,17 @@
+@extends('webtv.home.base')
+
+@section('title','webtv')
+
+@section('content')
+  <div class="p-2 mt-28   lg:flex" >
+    <section class="w-full bg-gray-500 h-96 lg:w-[45%] lg:h-full" >
+
+    </section>
+    <section class="grid  sm:grid-cols-2 lg:grow" >
+        <div class="w-full bg-yellow-800 h-[18rem] my-2 sm:mx-2 lg:my-0 lg:mb-2 sm:w-auto lg:h-52" ></div>
+        <div class="w-full bg-yellow-800 h-[18rem] my-2 sm:mx-2 lg:my-0 lg:mb-2 sm:w-auto lg:h-52" ></div>
+        <div class="w-full bg-yellow-800 h-[18rem] my-2 sm:mx-2 lg:my-0 lg:mb-2 sm:w-auto lg:h-52" ></div>
+        <div class="w-full bg-yellow-800 h-[18rem] my-2 sm:mx-2 lg:my-0 lg:mb-2 sm:w-auto lg:h-52" ></div>
+    </section>
+  </div>
+@endsection
